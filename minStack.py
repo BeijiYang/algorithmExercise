2364489b7b92617078166d9adb8370dd6b9dc938
BeijiYang <langyuyang@outlook.com>
2018-11-11 17:53:44 +0800

    a better python solution

--- minStack.py
+++ minStack.py
@@ -15,6 +15,7 @@
 # minStack.top();      --> Returns 0.
 # minStack.getMin();   --> Returns -2.
 
+# SOLUTION ONE
 class MinStack(object):
 
     def __init__(self):
@@ -51,3 +52,45 @@ class MinStack(object):
         """
         return min(self.stack)
         
+# A BETTER SOLUTION
+class MinStack(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.min = []
+        
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: void
+        """
+        self.stack.append(x)
+        if(len(self.min) > 0):
+            self.min.append(min([x, self.min[-1]]))
+        else:
+            self.min.append(x)
+        
+
+    def pop(self):
+        """
+        :rtype: void
+        """
+        self.stack.pop()
+        self.min.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
+        
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.min[-1]
