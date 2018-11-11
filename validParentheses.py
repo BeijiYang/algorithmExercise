fb07935e1fab819b5fb4fa5c706ffa5ee1a99772
BeijiYang <langyuyang@outlook.com>
2018-11-11 19:23:06 +0800

    add python solution of valid parentheses question

---
+++ validParentheses.py
@@ -0,0 +1,57 @@
+# Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+# An input string is valid if:
+
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+# Note that an empty string is also considered valid.
+
+# Example 1:
+
+# Input: "()"
+# Output: true
+# Example 2:
+
+# Input: "()[]{}"
+# Output: true
+# Example 3:
+
+# Input: "(]"
+# Output: false
+# Example 4:
+
+# Input: "([)]"
+# Output: false
+# Example 5:
+
+# Input: "{[]}"
+# Output: true
+
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        for i in xrange(len(s)):
+            cur = s[i]
+            if len(stack) == 0:
+                pre = None
+            else:
+                pre = stack[-1]
+            if self.match(pre, cur):
+                stack.pop()
+            else:
+                stack.append(cur)
+
+        if len(stack) == 0:
+            return True
+        else:
+            return False
+        
+    def match(self, pre, cur):
+        if (pre == "(" and cur == ")") or (pre == "[" and cur == "]") or (pre == "{" and cur == "}"):
+            return True
+        else:
+            return False
