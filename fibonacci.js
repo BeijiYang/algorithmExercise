290e443620dd8a292a33ca0b68d9a69b9ff77429
BeijiYang <langyuyang@outlook.com>
2018-11-13 18:04:41 +0800

    add a poor solution: recursion

--- fibonacci.js
+++ fibonacci.js
@@ -16,4 +16,11 @@ const fibonacci = function (n) {
 // a, b=>c       //a,b 往前平移了零次
 //    a, b=>c    //a,b 往前平移了一次
 //       a, b=>c //a,b 往前平移了两次
-// 0, 1, 1, 2, 3  
+// 0, 1, 1, 2, 3
+
+// 递归性能差
+// const fibonacci = function (n) {
+//   if (n === 1) return 0
+//   if (n === 2) return 1
+//   return fibonacci(n - 1) + fibonacci(n - 2)
+// }
