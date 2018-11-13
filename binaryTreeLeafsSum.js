70edecb1c4368ba8f5c546115d468feed37a1de0
BeijiYang <langyuyang@outlook.com>
2018-11-13 23:57:33 +0800

    fix a bug

--- binaryTreeLeafsSum.js
+++ binaryTreeLeafsSum.js
@@ -6,10 +6,11 @@ class TreeNode {
   }
 }
 
-const getLeafsSum = (node) => ((!node.left && !node.right) ? node.value : getLeafsSum(node.left) + getLeafsSum(node.right))
+const getLeafsSum = node => (node ? (!node.left && !node.right) ? node.value : getLeafsSum(node.left) + getLeafsSum(node.right) : 0)
 /*
 // normal code here
 const getLeafsSum = (node) => {
+  if (!node) return 0
   if (!node.left && !node.right) return node.value
   return getLeafsSum(node.left) + getLeafsSum(node.right)
 }
@@ -18,12 +19,10 @@ const node3 = new TreeNode(3)
 const node1 = new TreeNode(1)
 const node6 = new TreeNode(6)
 const node4 = new TreeNode(4)
-const node7 = new TreeNode(7)
 
 
 node3.left = node1
 node3.right = node6
 node6.left = node4
-node6.right = node7
 
 console.log(getLeafsSum(node3))
