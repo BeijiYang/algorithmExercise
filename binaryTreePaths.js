67b95b20eaea044a2f8cdd681eb86e605b0bdb94
BeijiYang <langyuyang@outlook.com>
2018-11-16 18:18:18 +0800

    fix a bug: number => string

--- binaryTreePaths.js
+++ binaryTreePaths.js
@@ -1,11 +1,7 @@
 // Given a binary tree, return all root - to - leaf paths.
-
 // Note: A leaf is a node with no children.
-
 // Example:
-
 // Input:
-
 //      1
 //    /   \
 //   2     3
@@ -14,7 +10,6 @@
 
 // Output: ["1->2->5", "1->3"]
 // Explanation: All root - to - leaf paths are: 1 -> 2 -> 5, 1 -> 3
-
 /**
  * Definition for a binary tree node.
  * function TreeNode(val) {
@@ -30,7 +25,7 @@ const binaryTreePaths = (root) => {
   let res = []
   const pathLoger = (node, prePath = '') => {
     if (!node) return
-    let curPath = prePath ? `${prePath}->${node.val}` : node.val
+    let curPath = prePath ? `${prePath}->${node.val}` : `${node.val}` // make it a string
     if (!node.left && !node.right) {
       res.push(curPath)
     }
