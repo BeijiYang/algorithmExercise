c283123cd524e25aa716e1478fa90bb1f5233876
BeijiYang <langyuyang@outlook.com>
2018-11-17 15:46:06 +0800

    make code cleaner

--- identicalBinaryTree.js
+++ identicalBinaryTree.js
@@ -18,9 +18,5 @@ var isSameTree = function (p, q) {
   if (!p && !q) return true
   if (p && !q || !p && q) return false
 
-  if (p.val === q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right)) {
-    return true
-  } else {
-    return false
-  }
+  return p.val === q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
 };
