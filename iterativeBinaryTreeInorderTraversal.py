244eafa4cd40bafdb46861ca0d76fadd47a9848a
BeijiYang <langyuyang@outlook.com>
2018-11-17 18:41:04 +0800

    add a py solution

---
+++ iterativeBinaryTreeInorderTraversal.py
@@ -0,0 +1,42 @@
+#  Given a binary tree, return the inorder traversal of its nodes' values.
+
+#  Recursive solution is trivial, could you do it iteratively ?
+
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: A Tree
+    @return: Inorder in ArrayList which contains node values.
+    """
+    def inorderTraversal(self, root):
+        # write your code here
+        stack = []
+        res = []
+        root = self.handleLeftTree(root, stack, res)
+        while root:
+            if root.right:
+                root = root.right
+                root = self.handleLeftTree(root, stack, res)
+            else:
+                if len(stack) > 0:
+                    root = stack.pop(-1)
+                    res.append(root.val)
+                else: 
+                    root = None
+        return res
+        
+    def handleLeftTree(self, root, stack, res):
+        if root is None:
+            return
+        while root.left:
+            stack.append(root)
+            root = root.left
+        res.append(root.val)
+        return root
