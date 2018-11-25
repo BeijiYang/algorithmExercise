9fd4216e7779509f1a7b8581b8baa0084b0cc1d5
BeijiYang <langyuyang@outlook.com>
2018-11-25 19:58:47 +0800

    merge sorted arrays

---
+++ mergeTwoSortedArrays.js
@@ -0,0 +1,36 @@
+// Description
+// Merge two given sorted integer array A and B into a new sorted integer array.
+
+//   Example
+// A = [1, 2, 3, 4]
+// B = [2, 4, 5, 6]
+// return [1, 2, 2, 3, 4, 4, 5, 6]
+
+/**
+ * @param A: sorted integer array A
+ * @param B: sorted integer array B
+ * @return: A new sorted integer array
+ */
+const mergeSortedArray = function (A, B) {
+  let left = 0
+  let right = 0
+  let C = []
+  while (left < A.length && right < B.length) {
+    if (A[left] < B[right]) {
+      C.push(A[left])
+      left++
+    } else {
+      C.push(B[right])
+      right++
+    }
+  }
+  if (left < A.length) {
+    let t = A.slice(left)
+    C.concat(t)
+  }
+  if (right < B.length) {
+    let t = B.slice(right)
+    C.concat(t)
+  }
+  return C
+}
