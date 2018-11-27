490962250e70b899caf1ec825842cd03ffd04275
BeijiYang <langyuyang@outlook.com>
2018-11-27 18:42:02 +0800

    py: partition array

---
+++ partition.py
@@ -0,0 +1,32 @@
+# Partition Array
+# Given an array nums of integers and an int k, partition the array (i.e move the elements in "nums") such that:
+
+# All elements < k are moved to the left
+# All elements >= k are moved to the right
+# Return the partitioning index, i.e the first index i nums[i] >= k.
+
+# Example
+# If nums = [3,2,2,1] and k=2, a valid answer is 1.
+
+class Solution:
+    """
+    @param nums: The integer array you should partition
+    @param k: An integer
+    @return: The index after partition
+    """
+    def partitionArray(self, nums, k):
+        # write your code here
+        left = 0
+        right = len(nums) - 1
+        
+        while left <= right:
+            while left <= right and nums[left] < k:
+                left += 1
+            while left <= right and nums[right] >= k: # 审题
+                right -= 1
+            if left <= right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1 
+                right -= 1
+            
+        return left
