78b77c4188ae19c1e190c0afab559ba873850f96
BeijiYang <langyuyang@outlook.com>
2018-11-27 12:11:58 +0800

    add lintcode solution

--- mergeSort.js
+++ mergeSort.js
@@ -4,7 +4,7 @@
 // Example
 // Given[3, 2, 1, 4, 5], return [1, 2, 3, 4, 5].
 
-var sortIntegers2 = function (A) {
+var sortIntegers2 = (A) => {
   if (A.length <= 1) return A // incase of []
   let mid = Math.floor(A.length / 2)
   let left = A.slice(0, mid)
@@ -30,3 +30,39 @@ var merge = (arrLeft, arrRight) => {
 }
 
 console.log(sortIntegers2([3, 2, 1, 4, 5]))
+
+
+// for lintcode
+var sortIntegers2 = function (A) {
+  if (A.length <= 1) {
+    return A
+  }
+  let mid = Math.floor(A.length / 2)
+  let left = A.slice(0, mid)
+  let right = A.slice(mid)
+  let temp = []
+
+  return merge(sortIntegers2(left), sortIntegers2(right), temp, A)
+}
+
+var merge = (arrLeft, arrRight, temp, A) => {
+  let i = 0
+  let j = 0
+
+  while (i < arrLeft.length && j < arrRight.length) {
+    if (arrLeft[i] < arrRight[j]) {
+      temp.push(arrLeft[i])
+      i++
+    } else {
+      temp.push(arrRight[j])
+      j++
+    }
+  }
+  temp = temp.concat(arrLeft.slice(i)).concat(arrRight.slice(j))
+
+  // for lintcode
+  for (let idx = 0; idx < A.length; idx++) {
+    A[idx] = temp[idx]
+  }
+  return temp
+}
