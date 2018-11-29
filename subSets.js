76483b6f5a7ad8b6545c2c0bd81d79dd0d50672b
BeijiYang <langyuyang@outlook.com>
2018-11-29 17:56:35 +0800

    add comments

--- subSets.js
+++ subSets.js
@@ -18,7 +18,7 @@
  * @return: A list of lists
  */
 const subsetHelper = (nums, subset, result, startIndex) => {
-  result.push(subset.slice(0))
+  result.push(subset.slice(0))) // 创建副本，及时捕获值。否则最后得到 m 个相同的[]。作用域的问题。
   for (let i = startIndex; i < nums.length; i++) {
     subset.push(nums[i])
     subsetHelper(nums, subset, result, i + 1)
@@ -30,6 +30,6 @@ const subsets = function (nums) {
   if (!nums) return []
   if (nums.length === 0) return [[]]
   let result = []
-  subsetHelper(nums.sort((a, b) => (a - b)), [], result, 0)
+  subsetHelper(nums.sort((a, b) => (a - b)), [], result, 0) // sort() 会按 ASKII 来排，2 > 10
   return result
 }
