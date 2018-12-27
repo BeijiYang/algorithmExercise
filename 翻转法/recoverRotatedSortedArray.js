c93af8615ad3d06ec67354511230d784a2334334
BeijiYang <langyuyang@outlook.com>
2018-12-27 11:59:35 +0800

    better solution

--- 翻转法/recoverRotatedSortedArray.js
+++ 翻转法/recoverRotatedSortedArray.js
@@ -10,25 +10,46 @@
  * @param nums: An integer array
  * @return: 
  */
+const findMinIndex = nums => {
+  let minIndex
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] > nums[i + 1]) minIndex = i + 1
+  }
+  return minIndex ? minIndex : 0
+}
+Array.prototype.reverseArray = function () {
+  // const reverseArray = nums => {
+  let nums = this
+  let start = 0
+  let end = nums.length - 1
+  let temp
+  while (start < end) {
+    temp = nums[start]
+    nums[start] = nums[end]
+    nums[end] = temp
+    start++
+    end--
+  }
+  return nums
+}
+
 const recoverRotatedSortedArray = function (nums) {
   if (!Array.isArray(nums)) return false
   if (nums.length == 0) return nums
-
-  console.log('-------')
   // find the min
-  let minIndex
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] > nums[i + 1]) minIndex = i + 1
-  }
-
-  console.log(minIndex)
+  let minIndex = findMinIndex(nums)
   // reverse reverse reverse
-  let frontArr = nums.slice(0, minIndex).reverse()
-  let endArr = nums.slilce(minIndex).reverse()
-  let wholeArr = frontArr.concat(endArr).reverse()
-  return wholeArr
+  let frontArr = nums.slice(0, minIndex).reverseArray()
+  let endArr = nums.slice(minIndex).reverseArray()
+  let wholeArr = frontArr.concat(endArr).reverseArray()
+
+  for (let i = 0; i < wholeArr.length; i++) {
+    nums[i] = wholeArr[i]
+  }
+  return nums
 }
-
-console.log(recoverRotatedSortedArray([4, 5, 1, 2, 3]))
-
+let nums = [4, 5, 1, 2, 3]
+// let nums = [1, 2, 3]
+// let nums = [2, 3, 1]
+console.log(recoverRotatedSortedArray(nums))
 
