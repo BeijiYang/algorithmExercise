c7b2a8d5f8e79f04a12881ea835183a07a2952fd
BeijiYang <langyuyang@outlook.com>
2019-01-04 23:47:56 +0800

    better style

--- binaryTree/maxDepthOfBinaryTree.js
+++ binaryTree/maxDepthOfBinaryTree.js
@@ -6,8 +6,8 @@ const maxDepth = root => (root ? ((!root.left && !root.right) ? 1 : Math.max(max
 
 // normal code style
 const maxDepth = function (root) {
-  if (!root) return 0
-  if (!root.left && !root.right) return 1
+  if (root == null) return 0
+  // if (!root.left && !root.right) return 1
   return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1
 }
 
