a7120666290f96876e0c475dc463b9b117ddb274
BeijiYang <langyuyang@outlook.com>
2019-01-04 23:30:54 +0800

    add comment

--- binaryTree/maxDepthOfBinaryTree.js
+++ binaryTree/maxDepthOfBinaryTree.js
@@ -10,3 +10,10 @@ const maxDepth = function (root) {
   if (!root.left && !root.right) return 1
   return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1
 }
+
+/*
+分治法
+整棵树的高度为 较高的子树的高度+1
+分为 左子树的高度 与 右子树的高度
+细分到当一颗树根节点为空时，高度为零
+*/
