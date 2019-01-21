a5290585efd46c24bbde325fb41cff4159386179
BeijiYang <langyuyang@outlook.com>
2019-01-21 19:00:54 +0800

    go over BFS

--- binaryTree/binaryTreeLevelOrderTraversal.js
+++ binaryTree/binaryTreeLevelOrderTraversal.js
@@ -51,16 +51,18 @@ var levelOrder = function (root) {
   }
   return res
 };
-
-// BFS 骨架
-var BFS = function (root) {
+/**
+ * level order 双重循环。第一重队列不空，第二重即 for 当前层，找出下一层的点
+ */
+// BFS 模板
+const BFS = root => {
   const queue = []
-  q.push(root)
+  queue.push(root)
 
-  while (q.length != 0) {
-    const len = q.length
+  while (queue.length != 0) {
 
-    for (let i = 0; i < len; i++) {
+    const len = queue.length // 重要操作！因为在下面的循环里，队列的长度是变化的。必须先取出来
+    for (let i = 0; i < len; i++) { // 从队列里取出当前层的点
       const node = queue.shift()
       if (node) {
 
@@ -70,3 +72,28 @@ var BFS = function (root) {
     }
   }
 };
+
+// 二刷
+const levelOrder = function (root) {
+  const reslist = []
+  const queue = []
+  queue.push(root)
+
+  while (queue.length !== 0) {
+    let layer = []
+    let len = queue.length
+
+    for (let i = 0; i < len; i++) {
+      let node = queue.shift()
+      if (node) {
+        layer.push(node.val)
+        if (node.left) queue.push(node.left)
+        if (node.right) queue.push(node.right)
+      }
+      // console.log(layer)
+
+    }
+    if (layer.length > 0) reslist.push(layer)
+  }
+  return reslist
+}
