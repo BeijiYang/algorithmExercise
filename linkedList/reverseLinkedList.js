213f431a24676c8ee32a4921e859019fbb178d42
BeijiYang <langyuyang@outlook.com>
2019-01-27 16:36:29 +0800

    reverse linked list

---
+++ linkedList/reverseLinkedList.js
@@ -0,0 +1,25 @@
+// 206. Reverse Linked List
+// Easy
+
+// Reverse a singly linked list.
+
+//   Example:
+// Input: 1 -> 2 -> 3 -> 4 -> 5 -> NULL
+// Output: 5 -> 4 -> 3 -> 2 -> 1 -> NULL
+/**
+ * @param head: n
+ * @return: The new head of reversed linked list.
+ */
+const reverse = head => {
+  let prev = null
+  let curt = head
+
+  while (curt !== null) {
+    let temp = curt.next
+    curt.next = prev
+    prev = curt
+    curt = temp
+  }
+  // curt == null
+  return prev
+}
